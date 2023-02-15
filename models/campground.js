02a1d057213dd20ec3ea90e94541c5265e74ca0a
Lovy99 <362809422@qq.com>
2023-02-14 23:15:14 -0800

    image upload finish

--- models/campground.js
+++ models/campground.js
@@ -2,14 +2,34 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
+
+const opts = { toJSON: { virtuals: true } };
+
 const CampgroundSchema = new Schema({
     title: String,
+    images: [ImageSchema],
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
     price: Number,
     description: String,
-    images: [{
-        url: String,
-        filename: String
-    }],
     location: String,
     author: {
         type: Schema.Types.ObjectId,
@@ -21,7 +41,15 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+}, opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`
 });
+
+
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
