645419e7ae5febe391020de16b9de20ba003f1ef
Lovy99 <362809422@qq.com>
2023-02-19 23:56:48 -0800

    add a url

--- app.js
+++ app.js
@@ -115,6 +115,7 @@ const styleSrcUrls = [
     "https://fonts.googleapis.com/",
     "https://use.fontawesome.com/",
     "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+    "https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
 ];
 const connectSrcUrls = [
     "https://api.mapbox.com/",
